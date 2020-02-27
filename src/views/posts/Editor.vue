3697e6760f948dfc88b4be186621411ae0f45c40
Ardika Rommy Sanjaya <contact@ardikars.com>
2020-02-27 20:57:51 +0700

    Fix image size

--- src/views/posts/Editor.vue
+++ src/views/posts/Editor.vue
@@ -151,8 +151,8 @@ export default {
     },
     handleThumbnailImageAdded: function () {
       const file = this.$refs.thumbnailImage.files[0]
-      const width = 500
-      const height = 300
+      const width = 1920
+      const height = 917
       var parent = this
       this.imageResizedToBase64(file, width, height, async function (data) {
         const response = await storeageService.upload(data)
