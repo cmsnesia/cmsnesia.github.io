063811af9c1288becf5d3ea3a1d2cf1519ba0b8a
Ardika Rommy Sanjaya <contact@ardikars.com>
2020-03-05 23:06:09 +0700

    Fix eslint

--- src/service/ProfileService.js
+++ src/service/ProfileService.js
@@ -2,21 +2,20 @@
 import HttpService from './HttpService'
 
 export default class ProfileService extends HttpService {
-    
     static api = process.env.VUE_APP_PROFILE_API
 
     add (data) {
-       const api = this.api + 'add'
-       return this.post(data, api)
+      const api = this.api + 'add'
+      return this.post(data, api)
     }
 
     edit (data) {
-        const api = this.api + 'edit'
-        return this.put(data, api)
+      const api = this.api + 'edit'
+      return this.put(data, api)
     }
 
     findById () {
-        const api = this.api + 'findById'
-        return this.get({}, api)
+      const api = this.api + 'findById'
+      return this.get({}, api)
     }
 }
